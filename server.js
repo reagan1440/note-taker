37bd9671d7c7d53e19908c7b5d88540e4966e076
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-16 23:01:29 -0400

    uuid

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Develop/routes/apiRoutes.js');
+const htmlRoutes = require('./Develop/routes/htmlRoutes.js');
 
 const app = express ();
 
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Now listening on PORT: ${PORT}`);
